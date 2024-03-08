2118222766a368c5290da8d42cc6501ef17614c0
wxxd-fxrest <fxrest704@gmail.com>
2024-03-08 15:29:33 +0900

    각각의 버튼들을 컬렉션으로 묶어 연결

--- calculatorApp/HeaderController.swift
+++ calculatorApp/HeaderController.swift
@@ -8,36 +8,122 @@
 import UIKit
 
 class HeaderController: UIViewController {
+    // number Label
+    @IBOutlet weak var numberLabel: UILabel!
 
-    @IBOutlet weak var ACButton: UIButton!
-    @IBOutlet weak var PlusMinusButton: UIButton!
-    @IBOutlet weak var FountainButton: UIButton!
-    
-    @IBAction func ACButtonClick(_ sender: UIButton) {
-        print("ACButtonClick click")
+    // Header Button Collection
+    @IBOutlet var headerButtons: [UIButton]!
+    @IBAction func headerButtonClicked(_ sender: UIButton) {
+        if let index = headerButtons.firstIndex(of: sender) {
+            switch index {
+            case 0:
+                print("ACButton click")
+            case 1:
+                print("PlusMinusButton click")
+            case 2:
+                print("FountainButton click")
+            default:
+                break
+            }
+        }
     }
     
-    @IBAction func PlusMinusButtonClick(_ sender: UIButton) {
-        print("PlusMinusButton click")
+    // Right Button
+    @IBOutlet var rightButtons: [UIButton]!
+    @IBAction func rightButtonClicked(_ sender: UIButton) {
+        if let index = rightButtons.firstIndex(of: sender) {
+            switch index {
+            case 0:
+                print("DivisionButton click")
+            case 1:
+                print("MultiplicationButton click")
+            case 2:
+                print("MinusButton click")
+            case 3:
+                print("PlusButton click")
+            case 4:
+                print("ResultButton click")
+            default:
+                break
+            }
+        }
     }
     
-    @IBAction func FountainButtonClick(_ sender: UIButton) {
-        print("FountainButton click")
+    // Number Button
+    @IBOutlet var numberButtons: [UIButton]!
+    @IBAction func numberButtonClicked(_ sender: UIButton) {
+        if let index = numberButtons.firstIndex(of: sender) {
+            switch index {
+            case 0:
+                print("1 click")
+            case 1:
+                print("2 click")
+            case 2:
+                print("3 click")
+            case 3:
+                print("4 click")
+            case 4:
+                print("5 click")
+            case 5:
+                print("6 click")
+            case 6:
+                print("7 click")
+            case 7:
+                print("8 click")
+            case 8:
+                print("9 click")
+      
+            default:
+                break
+            }
+        }
     }
+
+    
+    @IBOutlet weak var ZeroButton: UIButton!
+    @IBOutlet weak var SpotButton: UIButton!
+
+    // Zero Button
+    @IBAction func ZeroButtonClick(_ sender: UIButton) {
+        print("ZeroButton click")
+    }
+
+    // Spot Button
+    @IBAction func SpotButtonClick(_ sender: UIButton) {
+        print("SpotButton click")
+    }
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Header Button
+        for button in headerButtons {
+            button.layer.cornerRadius = button.bounds.width / 2
+            button.clipsToBounds = true
+        }
         
-        // 버튼의 너비의 절반을 radius로 설정하여 완전한 원형으로 만듦
-        self.ACButton.layer.cornerRadius = self.ACButton.bounds.width / 2
-        self.PlusMinusButton.layer.cornerRadius = self.PlusMinusButton.bounds.width / 2
-        self.FountainButton.layer.cornerRadius = self.FountainButton.bounds.width / 2
+        // Right Button
+        for button in rightButtons {
+            button.layer.cornerRadius = button.bounds.width / 2
+            button.clipsToBounds = true
+        }
+        
+        // Number Button
+        for button in numberButtons {
+            button.layer.cornerRadius = button.bounds.width / 2
+            button.clipsToBounds = true
+        }
 
-        // 버튼의 경계를 벗어나는 부분은 잘라냄
-        self.ACButton.clipsToBounds = true
-        self.PlusMinusButton.clipsToBounds = true
-        self.FountainButton.clipsToBounds = true
+        // Spot Button
+        self.SpotButton.layer.cornerRadius = self.SpotButton.bounds.width / 2
+        self.SpotButton.clipsToBounds = true
+
+        // Zero Button
+        let minSide = min(self.ZeroButton.bounds.width, self.ZeroButton.bounds.height)
+        self.ZeroButton.layer.cornerRadius = minSide / 2 // 버튼의 너비와 높이 중 작은 값의 절반을 radius로 설정하여 원형으로 만듦
+        self.ZeroButton.clipsToBounds = true // 버튼의 경계를 벗어나는 부분을 잘라냄
     }
 }
 
